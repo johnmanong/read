e0c8f93f83ef095fa0baf1d3fc8bef6865623ddd
johnmanong <johnmanong@gmail.com>
2015-01-16 01:01:04 -0500

    add takana; some styling

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,13 +46,19 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
-
+  grunt.loadNpmTasks('grunt-takana');
 
   grunt.registerTask('default', [
     'sass:dev',
     'connect:server',
     'watch'
-    ])
+  ]);
+
+  grunt.registerTask('tk', [
+    'connect:server',
+    'watch'
+  ])
+
 };
 
 
